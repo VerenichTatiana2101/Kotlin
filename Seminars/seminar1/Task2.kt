68a25a1f53a6264e4df98fd84597017998793e5e
Verenich Tatiana <verenik91@gmail.com>
2024-03-04 13:34:06 +0300

    решение простых задач, kotlin

---
+++ Seminars/seminar1/Task2.kt
@@ -0,0 +1,82 @@
+package seminar1
+
+fun main() {
+    /*
+    Реализовать функции sumAll, createOutputString и multiplyBy
+    так, чтобы программа выводила следующие строчки.
+    sumAll = 26
+    sumAll = 0
+    sumAll = 27
+    Alice has age of 42
+    Bob has age of 23
+    student Carol has age of 19
+    Daniel has age of 32
+    null
+    12
+     */
+    println("sumAll = ${sumAll(1, 5, 20)}")
+    println("sumAll = ${sumAll()}")
+    println("sumAll = ${sumAll(2, 3, 4, 5, 6, 7)}")
+
+    println(createOutputString("Alice"))
+    println(createOutputString("Bob", 23))
+    println(createOutputString(isStudent = true, name = "Carol", age = 19))
+    println(createOutputString("Daniel", 32, isStudent = null))
+
+    println(multiplyBy(null, 4))
+    println(multiplyBy(3, 4))
+}
+
+/**
+ * Принимает переменное число аргументов типа Int. Возвращает сумму
+ * всех чисел, либо 0, если не передан ни один аргумент.
+ */
+private fun sumAll(vararg numbers: Int): Int {
+    if (numbers.isEmpty()) return 0
+    else {
+        var sum = 0
+        for (element in numbers) {
+            sum += element
+        }
+        return sum
+    }
+}
+
+private fun sumAll2(vararg numbers: Int): Int {
+    return numbers.sum()
+}
+
+/**
+ * Формирует строку, используя параметры name, age и
+ * isStudent. У параметров age и isStudent есть значения по умолчанию.
+ */
+private fun createOutputString(
+    name: String,
+    age: Int = 42, isStudent: Boolean? = false
+): String {
+    return if (isStudent == true) "student $name has age of $age"
+    else "$name has age of $age"
+}
+
+private fun createOutputString2(
+    name: String,
+    age: Int = 42, isStudent: Boolean? = null
+): String =
+    when (isStudent) {
+        true -> "student $name has age of $age"
+        else -> "$name has age of $age"
+    }
+
+/**
+ * Принимает два числа типа Int и возвращает их произведение.
+ * Вместо первого числа, можно передать null, в этом случае функция должна
+ * вернуть null.
+ */
+private fun multiplyBy(i: Int?, b: Int?): Int? {
+    return if (i != null && b != null) {
+        i * b
+    } else {
+        null
+    }
+}
+
---
+++ Seminars/seminar1/Task3.kt
@@ -0,0 +1,24 @@
+package seminar1
+
+/*
+Написать программу, выводящую на экран фигуру из звёздочек.
+a – количество звёздочек на первой строчке;
+b – количество строк от первой до центральной и от
+центральной до последней;
+c – количество звёздочек, на которое увеличивается
+последовательность с каждой строкой.
+ */
+fun main() {
+    arraysStars(5, 2, 2)
+}
+
+fun arraysStars(a: Int, b: Int, c: Int) {
+    val symbol = "*"
+    var count = a
+    for (i in 0..c * b) {
+        var image: String = ""
+        image += symbol.repeat(count)
+        println(image)
+        count = if (i < b) (count + c) else count -c
+    }
+}
